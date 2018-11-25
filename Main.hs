b51829929056638475eef81b9a1ed8ace7300f7b
Maria Mukhina <m.avocoder@gmail.com>
2018-11-25 13:40:30 +0300

    Compute average temperature

---
+++ Main.hs
@@ -0,0 +1,28 @@
+module Main where
+
+import Data.Fixed
+
+main :: IO ()
+main = 
+  putStrLn "Сколько дней" >>
+  (readLn :: IO Integer) >>=
+  \дни -> 
+    getTemperatures дни >>=
+    \ts -> 
+      putStrLn "Средняя температура" >>
+      putStrLn (show (avg ts))
+ 
+getTemperatures :: Integer -> IO [Centi]
+getTemperatures =
+ \days -> 
+   if days == 0
+   then return []
+   else  putStrLn "Какая температура (Цельсия)" >>
+   (readLn :: IO Centi) >>=
+     \t ->
+     getTemperatures (days - 1) >>=
+     \ts ->
+     return (t : ts)
+
+avg :: [Centi] -> Centi
+avg = \xs -> sum xs / fromIntegral (length xs) 
